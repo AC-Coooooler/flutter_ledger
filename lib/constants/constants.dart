1531318a80fc4c6307e369cc65f3a8b66a01b2db
Alex Li <github@alexv525.com>
2022-08-06 23:54:54 +0800

    ✨ Add money related checks

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -4,7 +4,21 @@
 
 import 'dart:convert';
 
+import 'package:flutter/services.dart';
+
 const JsonEncoder globalJsonEncoder = JsonEncoder.withIndent('  ');
+const JsonEncoder globalJsonEncoderWithoutIntent = JsonEncoder();
 
 const String urlScheme = r'http(s?)://';
 final RegExp urlRegExp = RegExp(urlScheme);
+
+/// Allows 5 digits for the integer, and 2 digits for after the decimal.
+///
+/// Examples:
+/// - 10000.01
+/// - 0.01
+const String moneyRawString = r'^(0|[1-9][0-9]{0,4})(\.\d{0,2})?$';
+final RegExp moneyRegExp = RegExp(moneyRawString);
+final TextInputFormatter moneyFormatter = TextInputFormatter.withFunction(
+  (o, n) => (n.text.isEmpty || moneyRegExp.hasMatch(n.text)) ? n : o,
+);
